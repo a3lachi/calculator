d7aebe7ad800e268ec03205dd62924b97667c65c
a3laxi <mad.aalachi@gmail.com>
2024-01-03 14:12:37 +0100

    adding buttons

--- Calculator.java
+++ Calculator.java
@@ -28,40 +28,53 @@ public class Calculator extends Application {
 
     @Override
     public void start(Stage stage) {
-        Label l = new Label("Hello, this is a calculator.");
+        
 
         
         // Button button0 = new Button("0");button0.setStyle("-fx-font-size: 20;");
         Btn button0 = new Btn("0",120,60,20);
         Button buttonVirgule = new Btn(",",60,60,20);
         Button buttonEqual = new Btn("=",60,60,20,"#fbbf24");
-
         Button button1 = new Btn("1",60,60,20);
         Button button2 = new Btn("2",60,60,20);
         Button button3 = new Btn("3",60,60,20);
         Button buttonPlus = new Btn("+",60,60,20,"#fbbf24");
-
         Button button4 = new Btn("4",60,60,20);
         Button button5 = new Btn("5",60,60,20);
         Button button6 = new Btn("6",60,60,20);
         Button buttonMinus = new Btn("-",60,60,20,"#fbbf24");
+        Button button7 = new Btn("7",60,60,20);
+        Button button8 = new Btn("8",60,60,20);
+        Button button9 = new Btn("9",60,60,20);
+        Button buttonMultiply = new Btn("x",60,60,20,"#fbbf24");
+        Button buttonC = new Btn("C",60,60,20);
+        Button buttonNegative = new Btn("+/-",60,60,20);
+        Button buttonModulo = new Btn("%",60,60,15);
+        Button buttonDivision = new Btn("÷",60,60,20,"#fbbf24");
 
         HBox hbx1 = new HBox();
         hbx1.getChildren().addAll(button0,buttonVirgule,buttonEqual);
-
-
         HBox hbx2 = new HBox();
         hbx2.getChildren().addAll(button1,button2,button3,buttonPlus);
-
         HBox hbx3 = new HBox();
         hbx3.getChildren().addAll(button4,button5,button6,buttonMinus);
+        HBox hbx4 = new HBox();
+        hbx4.getChildren().addAll(button7,button8,button9,buttonMultiply);
+        HBox hbx5 = new HBox();
+        hbx5.getChildren().addAll(buttonC,buttonNegative,buttonModulo,buttonDivision);
 
         
+        Label l = new Label("0");
+        l.setPrefHeight(30);
+        l.setPrefWidth(240);
+        StackPane headLabel = new StackPane();
+        headLabel.setStyle("-fx-padding: 20px; -fx-background-color: #475569; -fx-font-size: 30 ; -fx-text-fill: #f8fafc;");
+        headLabel.getChildren().add(l);
 
         VBox root = new VBox();
-        root.getChildren().addAll(hbx3,hbx2, hbx1);
+        root.getChildren().addAll(headLabel,hbx5,hbx4,hbx3,hbx2, hbx1);
 
-        Scene scene = new Scene(root, 240, 380);
+        Scene scene = new Scene(root, 240, 330);
         stage.setScene(scene);
         stage.show();
     }
